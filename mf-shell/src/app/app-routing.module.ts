b7cd222a670268d3282b0cdcaf02e8db13261c3d
Fernando <lgarcia@tredicom.com>
2023-04-21 12:06:13 -0600

    feat: :sparkles: Integrated mf-users to mf-shell

--- mf-shell/src/app/app-routing.module.ts
+++ mf-shell/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
+import { loadRemoteModule } from '@angular-architects/module-federation';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    loadChildren: () => loadRemoteModule({
+      type: 'module',
+      remoteEntry: 'http://localhost:4201/remoteEntry.js',
+      exposedModule: "./UsersModule"
+    }).then(m => m.UsersModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
