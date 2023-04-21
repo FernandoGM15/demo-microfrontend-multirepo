af270a42b71098d5acdd0278b39ffe5f03eb77d4
Fernando <lgarcia@tredicom.com>
2023-04-21 11:44:28 -0600

    chore: :package: Added mf-users webpack config

--- mf-users/webpack.config.js
+++ mf-users/webpack.config.js
@@ -1,15 +1,20 @@
-const { shareAll, withModuleFederationPlugin } = require('@angular-architects/module-federation/webpack');
+const {
+  shareAll,
+  withModuleFederationPlugin,
+} = require("@angular-architects/module-federation/webpack");
 
 module.exports = withModuleFederationPlugin({
-
-  name: 'mf-users',
+  name: "mfUsers",
 
   exposes: {
-    './Component': './src/app/app.component.ts',
+    "./UsersModule": "./src/app/pages/users/users.module.ts",
   },
 
   shared: {
-    ...shareAll({ singleton: true, strictVersion: true, requiredVersion: 'auto' }),
+    ...shareAll({
+      singleton: true,
+      strictVersion: true,
+      requiredVersion: "auto",
+    }),
   },
-
 });
