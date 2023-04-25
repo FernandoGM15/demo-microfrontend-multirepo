e10861aacbfb99c4770f5f3220736e0eea42b7d1
Fernando <lgarcia@tredicom.com>
2023-04-25 11:02:41 -0600

    feat: :sparkles: Added PubSub on mfShell

--- mf-shell/src/app/app.component.ts
+++ mf-shell/src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import PubSub from 'pubsub-js';
 
+interface ICommonProduct {
+  name: string;
+  email: string;
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'mf-shell';
+  count = 0;
+  private _users: ICommonProduct[] = []
+
+  ngOnInit(): void {
+    PubSub.subscribe('users', (msg, data) => {
+      this._users.push(data as ICommonProduct);
+      this.count++;
+      localStorage.setItem('users', JSON.stringify(this._users));
+    })
+  }
 }
